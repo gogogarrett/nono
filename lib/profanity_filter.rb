6455968975c637bdb382cd408ece26854cebdb72
Garrett Heinlen <heinleng@gmail.com>
2012-05-31 22:28:58 -0500

    trying to fix indendetions

--- lib/profanity_filter.rb
+++ lib/profanity_filter.rb
@@ -3,24 +3,24 @@ require "profanity_filter/banned_word"
 
 module ProfanityFilter
 
-	def self.included(base)
-		base.class_eval {
-			require 'sanitize' 
-			before_save :check_profanity
+  def self.included(base)
+    base.class_eval {
+      require 'sanitize' 
+      before_save :check_profanity
 
-			def raise_error
-				errors.add(:base, "Profanity found. This prevented the record from saving.")
-			end
+      def raise_error
+        errors.add(:base, "Profanity found. This prevented the record from saving.")
+      end
 
-			def check_profanity
-				banned_words = WORDS
-				if self.attributes.values.any? {|c| banned_words.include? Sanitize.clean(content).gsub(/\r\n\t/, "") }
-					self.raise_error
-					return false
-				end
-			end
-		}
-	end
+      def check_profanity
+        banned_words = WORDS
+        if self.attributes.values.any? {|c| banned_words.include? Sanitize.clean(content).gsub(/\r\n\t/, "") }
+          self.raise_error
+          return false
+        end
+      end
+    }
+  end
 end
 
 ActiveRecord::Base.send(:include, ProfanityFilter)
